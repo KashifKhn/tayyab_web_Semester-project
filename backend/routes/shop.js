2df593142aa8c803e2c050409f0d231b45ecc120
KashifKhn <kashifkhnx04@gmail.com>
2024-07-15 18:22:29 +0500

    feat: Add GET route for retrieving shop items

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -3,13 +3,22 @@ const Shop = require("../model/Shop");
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    const shop = await Shop.find();
+    res.status(200).json(shop);
+  } catch (er) {
+    res.status(500).send("some error occured");
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const { name, price, imgUrl } = req.body;
 
-    const shop = new Shop({name, price, imgUrl})
-    await shop.save()
-    res.status(200).json(shop)
+    const shop = new Shop({ name, price, imgUrl });
+    await shop.save();
+    res.status(200).json(shop);
   } catch (er) {
     res.status(400).send("some error occured");
   }
